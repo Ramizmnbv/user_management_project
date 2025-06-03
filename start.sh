a89fac4a8010bcae3c5c885cfe884a7284500b0d
Ramizmnbv <ramizmnbv@gmail.com>
2025-06-03 16:41:26 +0600

    auto scipts import:install delayed

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ echo "Starting server on PORT: $PORT"
 exec php -S 0.0.0.0:$PORT -t public
 php bin/console cache:clear --env=prod || true
 php bin/console assets:install public --env=prod || true
+php bin/console importmap:install || true
