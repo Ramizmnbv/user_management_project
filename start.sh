e9a62cd2be4ff28ebe18c1e7b002b2d35a462efb
Ramizmnbv <ramizmnbv@gmail.com>
2025-06-03 13:55:14 +0600

    Port hardcoded for Railway compatibility

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
+PORT=9000
 echo "Starting server on PORT: $PORT"
-
-if [ -z "$PORT" ]; then
-  echo "PORT is not set. Defaulting to 8080"
-  PORT=8080
-fi
-
 exec php -S 0.0.0.0:$PORT -t public
