4b02a8610e97d89b48559b0ed41ac91d4267bc1f
Ramizmnbv <ramizmnbv@gmail.com>
2025-06-03 16:14:19 +0600

    auto scipts delayed

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@
 PORT=9000
 echo "Starting server on PORT: $PORT"
 exec php -S 0.0.0.0:$PORT -t public
+php bin/console cache:clear --env=prod || true
+php bin/console assets:install public --env=prod || true
